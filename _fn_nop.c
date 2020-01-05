da68bdee26d0a8166921bbb7f380902d92624e82
omeinsotelo <omeinsotelo@gmail.com>
2020-01-05 22:56:25 +0000

    nop

---
+++ _fn_nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * _fn_nop - doesn’t do anything.
+ * @stack: head
+ * @line_number: line_number
+ *
+ * Return: nothing
+ */
+void _fn_nop(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+	(void) line_number;
+}
