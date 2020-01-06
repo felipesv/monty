bfb45261a9ad8731cc26482cad2f6a0c63ab4f12
Felipe Satizabal <1165@holbertonschool.com>
2020-01-06 00:34:32 +0000

    pchar

---
+++ _fn_pchar.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+/**
+ * _fn_pchar - doesn’t do anything.
+ * @stack: head
+ * @line_number: line_number
+ *
+ * Return: nothing
+ */
+void _fn_pchar(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL)
+	{
+		dprintf(STDERR_FILENO, "L%d: can't pchar, stack empty\n", line_number);
+		structGl.status = 1;
+		return;
+	}
+	if ((*stack)->n < 0 || (*stack)->n > 255)
+	{
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
+		structGl.status = 1;
+		return;
+	}
+	printf("%c\n", (*stack)->n);
+}
